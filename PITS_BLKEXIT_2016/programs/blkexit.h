59159beb53e35600cf29cf553acd7a35dfd9b753
Angela To <to.angelac@gmail.com>
2016-03-02 09:11:09 -0800

    fixed 1995 eof stop

--- PITS_BLKEXIT_2016/programs/blkexit.h
+++ PITS_BLKEXIT_2016/programs/blkexit.h
@@ -7,12 +7,12 @@
 #define EM_OK    0
 #define EM_ERR   1
 #define FILEOF   401
-#define ROWSIZE  64000
+#define ROWSIZE  64000 //(Byte-Size) Record Buffer
 
 
 //------------ Type Definitions  ------------//
 typedef int          	Int32; 
-typedef long long int   Int64;
+typedef long int        Int64;
 typedef unsigned int 	UInt32;
 typedef short        	Int16;
 typedef double       	Dub64;
@@ -25,7 +25,7 @@ typedef struct inmod_struct {
 
 struct tuple {
     Int32 result;
-    Int64 rows;
+    Int32 rows;
 };
 
 //------------ File Paths ------------//
@@ -33,7 +33,6 @@ char *import_dict = "dictionary/wordsEn.dict";
 char *export_dict = "dictionary/wordsEx.dict";
 char *inputs_path = "cfgs/input_all.cfg";
 char *master_path = "cfgs/master_41.cfg";
-
 
 
 //------------ Buffer Declarations  ------------//
@@ -49,6 +48,6 @@ Int32 HostRestart();
 Int32 CheckPoint();
 Int32 DBSRestart();
 Int32 InvalidCode();
-Int32 MakeRecord(int cols, long long int numrows);
+Int32 MakeRecord(Int32 cols, Int32 numrows);
 Int32 BLKEXIT(char *tblptr);
 Int32 CleanUp();
